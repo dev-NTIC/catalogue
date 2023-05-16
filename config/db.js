11bd274ba07859b49638137652b5bba397045391
Bourouba ilyes <dev.ntic@bomarecompany.com>
2023-05-16 16:02:20 +0100

    db

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+    host: "localhost",
+    user: "root",
+    database: "bomareco_catalogue",
+    password: "",
 });
 
 module.exports = pool.promise();
